ea272ddca6551552676282d08c85167a27e0ad84
Will Koster <jameswk2@illinois.edu>
2017-09-22 16:24:36 -0500

    Make some progress on filtering
    We still need to implement the slider, but all in due time

--- src/App.js
+++ src/App.js
@@ -9,34 +9,48 @@ import './App.css';
  */
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.data = null;
-    fetch("/getTweets")
-      .then((response) => {
-      return response.body;
-      })
-      .then((json) => {
-      this.data = json;
-      });
-  }
+    constructor(props) {
+        super(props);
+        this.data = null;
+        fetch("/getTweets")
+            .then((response) => {
+                    return response.json();
+                    })
+        .then((json) => {
+                this.data = json;
+                console.log(json);
+                this.forceUpdate();
+                });
+    }
 
-  handleChange(event) {
-  console.log(event);
-  }
 
-  render() {
-    return (
-      <div className="App">
-          <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React</h2>
-              <p> Hopefully this works </p>
-          </div>
-          <input onChange={(e) => {this.handleChange(e)}} />
-      </div>
-    );
-  }
+    filterInfo(filter_var)
+    {
+        if(this.data != null)
+        {
+            var filteredTwitter = this.data.filter(tweet => tweet.retweet_count >= filter_var);
+            return filteredTwitter;
+        }
+        return [];
+    }
+
+    handleChange(event) {
+        console.log(event);
+    }
+
+    render() {
+        return (
+                <div className="App">
+                <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React</h2>
+                <p> Hopefully this works </p>
+                {this.filterInfo(1).map((number) => <p key={number.id}>{number.text}  {number.retweet_count}</p>)}
+                </div>
+                <input onChange={(e) => {this.handleChange(e)}} />
+                </div>
+               );
+    }
 }
 
 export default App;
