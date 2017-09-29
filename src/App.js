0e25d3baf7b7d5169f6b01cdf5f5022f694727f2
Yuxi Gu <yuxigu2@illinois.edu>
2017-09-29 12:00:11 -0500

    added slider that can filter based on retweet_count

--- src/App.js
+++ src/App.js
@@ -1,56 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Slider, { Range } from 'rc-slider';
+// We can just import Slider or Range to reduce bundle size
+// import Slider from 'rc-slider/lib/Slider';
+// import Range from 'rc-slider/lib/Range';
+import 'rc-slider/assets/index.css';
 /*
  *TODO:
  *Build feed with fake twitter data [tweets.json]
  *Build slider than can filter fake twitter data [tweets.json]
  */
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.data = null;
-        fetch("/getTweets")
-            .then((response) => {
-                    return response.json();
-                    })
-        .then((json) => {
-                this.data = json;
-                console.log(json);
-                this.forceUpdate();
-                });
-    }
+ class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = { value: 0 };
+    this.data = null;
+    fetch("/getTweets")
+      .then((response) => {
+      return response.json();
+      })
+      .then((json) => {
+      this.data = json;
+      console.log(json);
+      this.forceUpdate();
+      });
+  }
 
 
-    filterInfo(filter_var)
+  filterInfo(filter_var)
+  {
+    if(this.data != null)
     {
-        if(this.data != null)
-        {
-            var filteredTwitter = this.data.filter(tweet => tweet.retweet_count >= filter_var);
-            return filteredTwitter;
-        }
-        return [];
+    var filteredTwitter = this.data.filter(tweet => tweet.retweet_count >= filter_var)
+    //console.log(this.data[0].favourites_count)
+    //console.log(filteredTwitter);
+    return filteredTwitter;
     }
+    return [];
+  }
 
-    handleChange(event) {
-        console.log(event);
-    }
+  handleChange(event) {
+  console.log(event);
+  }
 
-    render() {
-        return (
-                <div className="App">
-                <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-                <p> Hopefully this works </p>
-                {this.filterInfo(1).map((number) => <p key={number.id}>{number.text}  {number.retweet_count}</p>)}
-                </div>
-                <input onChange={(e) => {this.handleChange(e)}} />
-                </div>
-               );
-    }
+  onSliderChange = (value) => {
+    this.setState({
+      value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+              <div>
+                  <Slider onChange={this.onSliderChange}/>
+              </div>
+              <p> Hopefully this works </p>
+        {this.filterInfo(this.state.value).map((number) => <p key={number.id}>{number.text}  {number.retweet_count}</p>)}
+          </div>
+          <input onChange={(e) => {this.handleChange(e)}} />
+      </div>
+    );
+  }
 }
-
 export default App;
