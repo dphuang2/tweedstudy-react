5cede1cf4852d878084a1c2f724f92f4f3e67a7f
Will Koster <jameswk2@illinois.edu>
2017-11-02 16:31:15 -0500

    Implement closeness
    Also refactor some stuff.

--- src/TweetFilterer.js
+++ src/TweetFilterer.js
@@ -114,9 +114,13 @@ class TweetFilterer {
       return 0;
   }
 
-  getSentiment(tweet) {
-      let words = tweet.text.toLowerCase().replace(/[^\w\s]/g, "").split(" ");
+  getTextSentiment(str) {
+      let words = str.toLowerCase().replace(/[^\w\s]/g, "").split(" ");
       return words.map(this.getWordSentiment.bind(this)).reduce((x, y) => x + y);
+  }
+
+  getSentiment(tweet) {
+      return getTextSentiment(tweet.text);
   }
 
   getCelebrity(tweet) {
@@ -147,7 +151,69 @@ class TweetFilterer {
   }
 
   async getCloseness(tweet) {
-      // Load messages, and implement algorithm. Not quite trivial but doable
+      return await getUserCloseness(tweet.user);
+  }
+
+  async getUserCloseness(user) {
+      // I'm not really sure how we're going to get these. Assume magic
+      // Also implement caching for this at some point
+      let messages = JSON.parse(await fetch("/getMessages"));
+      if(messages.length == 0)
+          return 0;
+      let out = 0;
+      if(user.followers_count > 100000)
+          out -= 3;
+      else if(user.followers_count > 10000)
+          out -= 2;
+      else if(user.followers_count > 1000)
+          out -= 1;
+      else
+          out += 1;
+      let words = messages.map(message => message
+              .text.replace(/[^\w\s]/g, "")
+              .split(" "))
+          .reduce((x, y) => x.concat(y), []);
+      let wordCount = words.length;
+      if(wordCount > 1000)
+          out += 10;
+      else if(wordCount > 100)
+          out =+ 5;
+      else if(wordCount > 10)
+          out += 2;
+      else if(wordCount > 0)
+          out += 1;
+
+      let averageSentiment = words
+          .map(this.getWordSentiment.bind(this)).reduce((x, y) => x + y) / wordCount;
+      if(averageSentiment > 0)
+          out += 1;
+      else 
+          out -= 1;
+
+      let mostRecentMessage = messages
+          .reduce((x, y) => new Date(x.created_at) > new Date(y.created_at) ? x : y);
+      let timeAgo = new Date() - new Date(mostRecentMessage.created_at);
+      const MILLIS_IN_HOUR = 60*1000;
+      let hoursAgo = timeAgo / MILLIS_IN_HOUR;
+
+      const HOURS_IN_DAY = 24;
+      const HOURS_IN_WEEK = HOURS_IN_DAY * 7;
+      const HOURS_IN_MONTH = HOURS_IN_DAY * 30;
+      if(hoursAgo < 6)
+          out += 10;
+      else if(hoursAgo < HOURS_IN_DAY)
+          out += 5;
+      else if(hoursAgo < HOURS_IN_WEEK)
+          out += 2;
+      else if(hoursAgo < HOURS_IN_MONTH)
+          out += 1;
+
+      if(user.verified)
+          out -= 1;
+      else
+          out += 1;
+
+      return out;
   }
 }
 export { TweetFilterer, FREQUENCY, CELEBRITY, POPULARITY, CLOSENESS, SENTIMENT };
