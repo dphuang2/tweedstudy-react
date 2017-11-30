7fdc97fd37f5535c2791511e6ce1c0908b05498b
Dylan Huang <dylan.p.huang@gmail.com>
2017-11-30 17:31:19 -0600

    Fixed ignored merge conflict

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -13,51 +13,6 @@ var get_data = getData.get_data;
 var get_all_data_id = getData.get_all_data_id;
 var get_all_data_cursor = getData.get_all_data_cursor;
 
-exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.create_a_task = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.delete_a_task = function(req, res) {
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Task successfully deleted' });
-  });
-};
-=======
->>>>>>> apiRefactor
-
 // GET /getTweets
 exports.get_tweets = function(req, res) {
   var json = JSON.parse(require('fs').readFileSync('data/tweets.json', 'utf8'));
