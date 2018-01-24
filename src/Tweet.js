78f8e1b9d527bd550272453c3991adb4a4bf774d
cherylqian <cherylqian@outlook.com>
2018-01-23 21:40:33 -0600

    count and more styling

--- src/Tweet.js
+++ src/Tweet.js
@@ -20,18 +20,32 @@ class Tweet extends Component {
 
     let media = null;
     if (tweet.entities.media) {
-        let size = {}
-        media = <img className="img-responsive" width={tweet.entities.media[0].sizes.thumb.w * 5} src={tweet.entities.media[0].media_url} alt=""/>;
+        media = <img id="media-img" className="img-responsive img-rounded" width={tweet.entities.media[0].sizes.thumb.w * 5} src={tweet.entities.media[0].media_url} alt=""/>;
     }
+
+    let counts = (<p>
+        {
+            tweet.retweeted ?
+            <span style={{color: '#66ff99'}}><span id="counts" className="glyphicon glyphicon-retweet" aria-hidden="true"></span> {tweet.retweet_count}</span> :
+            <span><span id="counts" className="glyphicon glyphicon-retweet" aria-hidden="true"></span> {tweet.retweet_count}</span>
+        }
+
+        {
+            tweet.favorited ?
+            <span style={{color: '#ff6699'}}><span id="counts" className="glyphicon glyphicon-heart" aria-hidden="true"></span>{tweet.favorite_count}</span> :
+            <span><span id="counts" className="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>{tweet.favorite_count}</span>
+        }
+        </p>)
 
     return ( //<p>{this.props.text}</p>
         <div id="tweet" className="row">
             {retweet_status}
             <a className="col-xs-2 col-md-offset-1 col-md-1" href={tweet.user.url}><img className='profileImg img-circle' src={tweet.user.profile_image_url} alt="profile"/></a>
             <span className="col-xs-10">
-                <a href={tweet.user.url}><b>{tweet.user.name}</b></a> <span>@{tweet.user.screen_name}</span> • {time_difference}
+                <a href={tweet.user.url}><b>{tweet.user.name}</b></a> <span style={{color: '#808080'}}>@{tweet.user.screen_name} • {time_difference}</span>
                 <p>{tweet.full_text.slice(tweet.display_text_range[0], tweet.display_text_range[1])}</p>
                 {media}
+                {counts}
             </span>
         </div>
     );
